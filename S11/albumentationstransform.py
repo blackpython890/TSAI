7cfdafc22e08cfa8a3fa0b4adf3ea3fc72416c69
Jagat Nandan Prasad <62509653+jagatabhay@users.noreply.github.com>
2020-06-14 00:54:18 +0530

    Add files via upload

---
+++ S11/albumentationstransform.py
@@ -0,0 +1,29 @@
+from albumentations import Compose, Normalize, HorizontalFlip, VerticalFlip, Resize,Rotate , Cutout
+from albumentations.pytorch import ToTensor
+import torchvision.transforms as transforms
+import imgaug.augmenters as iaa
+
+
+class train_transforms():
+
+    def __init__(self):
+        self.albTrainTransforms = Compose([  # Resize(256, 256),
+            Rotate((-10.0, 10.0)),
+            HorizontalFlip(p=0.5),
+            #VerticalFlip(p=0.5),
+            #transforms.RandomCrop(size = [32,32], padding = 4),
+            Cutout(num_holes = 1, max_h_size = 8, max_w_size = 8),
+            Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5]),
+            ToTensor()
+        ])# this is train transforms
+
+        self.tran = transforms.Compose([transforms.RandomCrop(size=[32,32] , padding = 4 )])
+        self.aug = iaa.Fliplr(0.5)
+
+    print("REQUIRED LIBRARIES LOADED...")
+
+    def __call__(self, img):
+        img = np.array( img )
+        #img = self.tran(image = img )
+        img = self.albTrainTransforms(image=img)['image']
+        return img

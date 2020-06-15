7437785b74aaa5165f947850a006f5b18e3d214e
Jagat Nandan Prasad <62509653+jagatabhay@users.noreply.github.com>
2020-06-15 15:25:13 +0530

    Add files via upload

---
+++ S11/dataloader.py
@@ -0,0 +1,37 @@
+import torchvision.transforms as transforms
+import torchvision
+
+
+def datasetloader(albumentationstransform_train_transforms):
+    mean = (0.4914, 0.4822, 0.4465)
+    std = (0.2023, 0.1994, 0.2010)
+    
+    trans = transforms.Compose([ transforms.ToTensor(),
+                             transforms.Normalize(mean, std)
+                            ]) 
+    
+    trainset = torchvision.datasets.CIFAR10(root='./data', 
+                                        train=True,
+                                        download=True, 
+                                        transform = albumentationstransform_train_transforms )
+                                        
+    trainloader = torch.utils.data.DataLoader(trainset,
+                                          batch_size=512,
+                                          shuffle=True,
+                                          num_workers=4, 
+                                          pin_memory=True)
+    
+    
+    testset = torchvision.datasets.CIFAR10(root='./data', 
+                                           train=False,
+                                           download=True, 
+                                           transform=trans )
+                                       
+    testloader = torch.utils.data.DataLoader(testset, 
+                                             batch_size=512,
+                                             shuffle=False, 
+                                             num_workers=4, 
+                                             pin_memory=True)
+    
+    print("INFO : Trainloader and Testloader Done")
+    return trainloader , testloader

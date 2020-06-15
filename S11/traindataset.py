e6836561bce260831ce70afda0784d12f4f2c451
Jagat Nandan Prasad <62509653+jagatabhay@users.noreply.github.com>
2020-06-15 19:58:04 +0530

    Add files via upload

---
+++ S11/traindataset.py
@@ -0,0 +1,31 @@
+
+
+def traindataset(range_ , model , device , trainloader , optimizer , criterion_ , batchsize ):
+     for epoch in range(range_):  # loop over the dataset multiple times
+        
+        running_loss = 0.0
+        for i, data in enumerate(trainloader, 0):
+            # get the inputs
+            inputs, labels = data[0].to(device),data[1].to(device)
+            
+            # zero the parameter gradients
+            optimizer.zero_grad()
+            
+            # forward + backward + optimize
+            outputs = model(inputs)
+            loss = criterion(outputs, labels)
+            loss.backward()
+            optimizer.step()
+            
+            # print statistics
+            running_loss += loss.item()
+            minibatch = 50000//batchsize
+            if i % minibatch == (minibatch-1) :    # print every 2000 mini-batches
+               print('[%d, %5d] loss: %.3f' %(epoch + 1, i + 1, running_loss / (minibatch-1)  ))
+               running_loss = 0.0
+            
+
+
+
+print('Finished Training')        
+     

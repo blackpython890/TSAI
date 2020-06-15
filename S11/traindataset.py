763755e4305e7af8745af85365da6e10d4d00c75
Jagat Nandan Prasad <62509653+jagatabhay@users.noreply.github.com>
2020-06-15 20:14:40 +0530

    Add files via upload

--- S11/traindataset.py
+++ S11/traindataset.py
@@ -23,9 +23,8 @@ def traindataset(range_ , model , device , trainloader , optimizer , criterion_ 
             if i % minibatch == (minibatch-1) :    # print every 2000 mini-batches
                print('[%d, %5d] loss: %.3f' %(epoch + 1, i + 1, running_loss / (minibatch-1)  ))
                running_loss = 0.0
-            
 
 
-
-print('INFO : Finished Training')        
+    
+    print('INFO : Finished Training of Dataset ')                
      

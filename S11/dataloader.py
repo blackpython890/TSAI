7f1d873b8fbcadff80e79b8615ece6622331c3f4
Jagat Nandan Prasad <62509653+jagatabhay@users.noreply.github.com>
2020-06-15 16:21:07 +0530

    Add files via upload

--- S11/dataloader.py
+++ S11/dataloader.py
@@ -3,7 +3,7 @@ import torchvision.transforms as transforms
 import torchvision
 
 
-def datasetloader(albumentationstransform_train_transforms):
+def datasetloader(albumentationstransform_train_transforms , batchsize , numwork ):
     mean = (0.4914, 0.4822, 0.4465)
     std = (0.2023, 0.1994, 0.2010)
     
@@ -11,28 +11,29 @@ def datasetloader(albumentationstransform_train_transforms):
                              transforms.Normalize(mean, std)
                             ]) 
     
-    trainset = torchvision.datasets.CIFAR10(root='./data', 
-                                        train=True,
-                                        download=True, 
+    trainset = torchvision.datasets.CIFAR10(root = './data', 
+                                        train = True ,
+                                        train = True ,
+                                        download = True , 
                                         transform = albumentationstransform_train_transforms )
                                         
     trainloader = torch.utils.data.DataLoader(trainset,
-                                          batch_size=512,
-                                          shuffle=True,
-                                          num_workers=4, 
-                                          pin_memory=True)
+                                              batch_size = batchsize,
+                                              shuffle = True,
+                                              num_workers = numwork , 
+                                              pin_memory = True)
     
     
     testset = torchvision.datasets.CIFAR10(root='./data', 
-                                           train=False,
-                                           download=True, 
-                                           transform=trans )
+                                           train = False,
+                                           download = True, 
+                                           transform = trans )
                                        
     testloader = torch.utils.data.DataLoader(testset, 
-                                             batch_size=512,
-                                             shuffle=False, 
-                                             num_workers=4, 
-                                             pin_memory=True)
+                                             batch_size = batchsize ,
+                                             shuffle = False, 
+                                             num_workers = numwork , 
+                                             pin_memory = True)
     
     print("INFO : Trainloader and Testloader Done")
     return trainloader , testloader

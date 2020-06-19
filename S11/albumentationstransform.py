0419265873e738e7c45d0fad17c8e48b2eb9d6e5
Jagat Nandan Prasad <62509653+jagatabhay@users.noreply.github.com>
2020-06-19 19:24:29 +0530

    RandomCrop PadIfNeeded

--- S11/albumentationstransform.py
+++ S11/albumentationstransform.py
@@ -1,7 +1,5 @@
-from albumentations import Compose, Normalize, HorizontalFlip, VerticalFlip, Resize,Rotate , Cutout
+from albumentations import Compose, Normalize, HorizontalFlip, VerticalFlip, Resize, Rotate , Cutout , RandomCrop , PadIfNeeded
 from albumentations.pytorch import ToTensor
-import torchvision.transforms as transforms
-import imgaug.augmenters as iaa
 import numpy as np
 
 
@@ -11,20 +9,19 @@ class train_transforms():
         self.albTrainTransforms = Compose([  # Resize(256, 256),
             Rotate((-10.0, 10.0)),
             HorizontalFlip(p=0.5),
-            #VerticalFlip(p=0.5),
-            #transforms.RandomCrop(size = [32,32], padding = 4),
+            RandomCrop( height = 32 , width = 32 , p = 1.0 ) ,
+            PadIfNeeded( min_height = 4 , min_width = 4 ),
             Cutout(num_holes = 1, max_h_size = 8, max_w_size = 8),
             Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5]),
             ToTensor()
-        ])# this is train transforms
+        ])
+        
+        # this is train transforms
 
-        self.tran = transforms.Compose([transforms.RandomCrop(size=[32,32] , padding = 4 )])
-        self.aug = iaa.Fliplr(0.5)
 
     print("REQUIRED LIBRARIES LOADED...")
 
     def __call__(self, img):
         img = np.array( img )
-        #img = self.tran(image = img )
         img = self.albTrainTransforms(image=img)['image']
         return img

13d9ee9e0746c975eddaab26b50a66aba901a96d
Jagat Nandan Prasad <62509653+jagatabhay@users.noreply.github.com>
2020-06-28 14:15:13 +0530

    TrainModel

---
+++ S12/traindataset.py
@@ -0,0 +1,31 @@
+
+
+def traindataset(range_ , model , device , trainloader , optimizer , criterion_ , batchsize , scheduler_ = None ):
+     for epoch in range(range_):  # loop over the dataset multiple times
+        
+        running_loss = 0.0
+        for i, data in enumerate(trainloader, 0):
+            # get the inputs
+            inputs, labels = data[0].to(device),data[1].to(device)
+            
+            # zero the parameter gradients
+            optimizer.zero_grad()
+            
+            # forward + backward + optimize
+            outputs = model(inputs)
+            loss = criterion_(outputs, labels)
+            loss.backward()
+            
+            optimizer.step()
+            scheduler_.step()
+            
+            # print statistics
+            running_loss += loss.item()
+            #minibatch = 50000//batchsize
+            if i % 90 == 89 :    # print every 2000 mini-batches
+               print('[%d, %5d] loss: %.3f' %(epoch + 1, i + 1, running_loss / 89 ))
+               running_loss = 0.0
+
+
+    
+     print('INFO : Finished Training of Dataset ') 
